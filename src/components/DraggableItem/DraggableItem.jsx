d3ab2dd98047004645c5a646db76de99d1f26beb
Szadziu <mcj.mikolajczak@gmail.com>
2021-11-14 23:01:31 +0100

    feature // range slider can be moving now

--- src/components/DraggableItem/DraggableItem.jsx
+++ src/components/DraggableItem/DraggableItem.jsx
@@ -1,12 +1,39 @@
+import { useRef, useState } from "react";
 import * as P from "./parts";
 
-const DraggableItem = ({ onMouseDown, onMouseUp, onMouseMove, position }) => {
+const DraggableItem = ({ slider }) => {
+  const itemRef = useRef();
+  const [isDrag, setIsDrag] = useState(false);
+  const [position, setPosition] = useState(0);
+
+  const dragItem = (e) => {
+    startDrag();
+  };
+
+  const moveItem = (e) => {
+    const { left, right } = slider.current.getBoundingClientRect();
+    if (isDrag) {
+      setPosition(e.clientX - left - 12.5);
+    }
+    if (e.clientX < left) setPosition(0);
+    if (e.clientX > right) setPosition(right - left - 25);
+  };
+
+  const startDrag = () => {
+    setIsDrag(true);
+  };
+
+  const stopDrag = () => {
+    setIsDrag(false);
+  };
   return (
     <P.Item
+      onMouseDown={dragItem}
+      onMouseLeave={stopDrag}
+      onMouseUp={stopDrag}
+      onMouseMove={moveItem}
+      ref={itemRef}
       position={position}
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
-      onMouseMove={onMouseMove}
     >
       10
     </P.Item>
--- src/components/PropertySlider/PropertySlider.jsx
+++ src/components/PropertySlider/PropertySlider.jsx
@@ -4,44 +4,14 @@ import DraggableItem from "../DraggableItem/DraggableItem";
 import * as P from "./parts";
 
 const PropertySlider = ({ name }) => {
-  const dragRef = useRef(null);
-  const [isDrag, setIsDrag] = useState(false);
-  const [itemPosition, setItemPosition] = useState(0);
-
-  const moveItem = (e) => {
-    if (isDrag) {
-      console.log(e.clientX);
-    }
-  };
-
-  const dragItem = (e) => {
-    moveItem(e);
-  };
-
-  const dragStart = (e) => {
-    console.log(e.clientX);
-    setIsDrag(true);
-  };
-
-  const dragStop = () => {
-    setIsDrag(false);
-    console.log("stop");
-  };
-
-  // const elementX = dragRef.current.getBoundingClientRect().x;
+  const sliderRef = useRef(null);
 
   return (
     <>
       <P.Wrapper>
         <P.PropertyName>{name}</P.PropertyName>
-        <P.Slider>
-          <DraggableItem
-            position={itemPosition}
-            ref={dragRef}
-            onMouseDown={dragStart}
-            onMouseUp={dragStop}
-            onMouseMove={dragItem}
-          />
+        <P.Slider ref={sliderRef}>
+          <DraggableItem slider={sliderRef} />
         </P.Slider>
       </P.Wrapper>
     </>
